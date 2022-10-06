01589534daf002350a50af0890263eeccf3589f8
KirSpaceB <productivekirk@gmail.com>
2022-10-06 12:49:09 -0700

    modified: sketch.css modified: sketch.html modified: sketch.js

--- sketch.js
+++ sketch.js
@@ -1,55 +1,25 @@
-const DEFAULT_COLOR = '#333333';
-const DEFAULT_MODE = 'color';
-const DEFAULT_SIZE = 16;
-
-let currentColor = DEFAULT_COLOR;
-let currentMode = DEFAULT_MODE;
-let currentSize = DEFAULT_SIZE;
+const colorPicker = document.getElementById('colorPicker');
+const container = document.getElementById('container');
 
 
-function setCurrentColor(newColor) {
-    currentColor = newColor;
-}
 
-function setCurrentMode(newMode) {
-    activateButton(newMode);
-    currentMode = newMode;
-}
 
-function setCurrentSize(newSize) {
-    currentSize = newSize;
-}
 
-const colorPicker = document.querySelector('.dot');
 
-colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+function makeRows(rows,cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+    for (c = 0; c < (rows * cols); c++) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid-item');
+        grid.addEventListener('mousedown', function() {
+            grid.style.backgroundColor = colorPicker.value;
+        }) 
+        container.appendChild(grid);
+    };
+};
 
-function changeColor(e) {
-    if (e.type === 'mouse' && !mouseDown) return;
-    if(currentMode === 'color') {
-        e.target.style.backgroundColor = currentColor;
-    }
-}
 
-function setupGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-    for (let i = 0; i < size * size; i++) {
-        const gridElement = document.createElement('div')
-        gridElement.classList.add('grid-element')
-        gridElement.addEventListener('mouseover', changeColor)
-        gridElement.addEventListener('mousedown', changeColor)
-        grid.appendChild(gridElement)
-    }
-
-}
-
-window.onload = () => {
-    setupGrid(DEFAULT_SIZE)
-    activateButton(DEFAULT_MODE)
-}
+makeRows(16,16);
