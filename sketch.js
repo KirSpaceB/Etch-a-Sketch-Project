f30e73d54aad384a47a0ebd9750d5ddb54368150
KirSpaceB <productivekirk@gmail.com>
2022-10-08 16:20:19 -0700

    sketch.css sketch.html sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,56 @@
-const colorPicker = document.getElementById('colorPicker');
-const container = document.getElementById('container');
+const colorPickerMode = document.getElementById('colorPicker');
+
+var activeMode = 'colorPicker.value';
+
+function currentMode(mode) {
+    activeMode = mode;
+}
 
 
 
+function createGrid(size) {
+    let board = document.getElementById('board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    
+    gridSize = size * size;
 
+    for (i = 0; i < gridSize; i++) {
 
-
-function makeRows(rows,cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-
-    for (c = 0; c < (rows * cols); c++) {
         let grid = document.createElement('div');
-        grid.classList.add('grid-item');
-        grid.addEventListener('mousedown', function() {
-            grid.style.backgroundColor = colorPicker.value;
-        }) 
-        container.appendChild(grid);
-    };
+
+        grid.addEventListener('mousedown' && 'mouseover', colorPickerModeX)
+
+        grid.style.backgroundColor = 'white';
+
+        board.insertAdjacentElement('beforeend', grid);
+    }
+}
+
+function changeSize(input) {
+    if (input >= 2 || input <= 100) {
+        createGrid(input);
+    } else {
+        console.log('error, too many squares');
+    }
+}
+
+function resetGrid() {
+    board.innerHTML = '';
+}
+
+function colorPickerModeX() {
+    this.style.backgroundColor = colorPicker.value;
+
+    if (activeMode === 'black') {
+        this.style.backgroundColor = activeMode;
+    } else if (activeMode === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else if (activeMode === 'white') {
+        this.style.backgroundColor = 'white'
+    } else {
+        console.log('error');
+    }
 };
 
-
-
-makeRows(16,16);
+//were trying to create multiple functions to have different modes in out etch a sketch
